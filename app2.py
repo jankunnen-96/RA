753f7629fc083b9b6a24392fe5fffbaee62189d8
jankunnen-96 <76209402+jankunnen-96@users.noreply.github.com>
2025-02-24 15:34:34 +0100

    up

--- app2.py
+++ app2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import folium
-from folium.plugins import MarkerCluster, Fullscreen
+from folium.plugins import MarkerCluster, Fullscreen,BeautifyIcon
 from streamlit_folium import folium_static
 from collections import defaultdict
 
@@ -15,7 +15,9 @@ def load_data():
     unique_artists = data['artist'].unique()
     # Merge duplicate rows except for the artist column
     data = data.groupby(data.columns.difference([column_to_merge]).tolist(), as_index=False)[column_to_merge].agg(' | '.join)
+    data['artists']=data['artists'].str.replace('_'," | ")
     return data.sort_values('date'),unique_artists
+
 
 data,unique_artists = load_data()
 
@@ -49,7 +51,7 @@ with col3:
     selected_artists = st.multiselect("Select Artist(s)", unique_artists)
 
 # Convert date filters
-start_date = pd.to_datetime(selected_date_range[0])  
+start_date = pd.Timestamp.today().normalize()  
 end_date = pd.to_datetime(selected_date_range[1])
 
 # Apply Filters
@@ -67,18 +69,32 @@ grouped_data = defaultdict(list)
 for _, row in filtered_data.iterrows():
     key = (row['latitude'], row['longitude'])
     
-    popup_text = (
-        f'<div style="min-width:300px; max-width:500px;">'  
-        f"<b>{row['location']}: {row['date']} - {row['title']} </b><br>"
-        f"{row['artist']}<br>"
-        f"<details><summary><b><u>Click here for Full Lineup</u></b></summary>{row['artists']}</details><br>"  
-        f'</div>'
-    )
     
+    # Construct HTML with increased width and side-by-side image using flex layout.
+    popup_text = (
+            f'<div style="display: flex; align-items: center;">'
+                f'<div style="margin-right: 10px;">'
+                    f'<img src="https://github.com/jankunnen-96/RA/blob/main/ownimage.png" alt="Event Image" style="width:150px; height:auto;">'
+                f'</div>'
+                f'<div style="flex: 1;">'
+                    f"<b>{row['date'].date()} - {row['title']}</b><br>"
+                    f"{row['artist']}<br>"
+                    f"<details><summary><b><u>Click here for Full Lineup</u></b></summary>{row['artists']}</details><br>"
+                f'</div>'
+            f'</div>'
+        )
+
+    if not grouped_data[key]:
+        grouped_data[key].append(f'<div style="min-width:500px; max-width:900px; max-height:400px; overflow-y:auto;">')
+        grouped_data[key].append(f"<b style='font-size:16px;'>{row['location']}</b><br><br>")
+
     grouped_data[key].append(popup_text)
 
+for key in grouped_data:
+    grouped_data[key].append(f'</div>')
+
 # Create Fullscreen Folium Map
-m = folium.Map(location=[48.8566, 2.3522], zoom_start=2, tiles="CartoDB dark_matter")
+m = folium.Map(location=[48.8566, 2.3522], zoom_start=4, tiles="CartoDB dark_matter")
 
 # Add Fullscreen Control
 Fullscreen(position="topleft").add_to(m)
@@ -92,9 +108,13 @@ for (lat, lon), events in grouped_data.items():
     folium.Marker(
         location=[lat, lon],
         popup=folium.Popup(popup_text),
-        tooltip=f"{len(events)} event(s) here",
-        icon=folium.Icon(color="blue", icon="info-sign"),
-    ).add_to(marker_cluster)
+        icon=BeautifyIcon(
+            icon='bolt',
+            icon_shape='marker',
+            border_color='darkblue',
+            background_color='skyblue',
+            text_color='black',
+            inner_icon_style='font-size:12px;',number=len(events)-3)).add_to(marker_cluster)
 
 # Display Fullscreen Map
 st.markdown(
@@ -103,4 +123,4 @@ st.markdown(
     </style>""", unsafe_allow_html=True
 )
 
-folium_static(m, width=1200, height=700)
+folium_static(m, width=900, height=2000)
