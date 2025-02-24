b45e5d2b8f7423497ed4a3a8279b68f0648ebd5e
jankunnen-96 <76209402+jankunnen-96@users.noreply.github.com>
2025-02-24 19:56:50 +0100

    update

--- app.py
+++ app.py
@@ -130,10 +130,10 @@ from user_agents import parse
 ua_string = st_javascript("""window.navigator.userAgent;""")
 user_agent = parse(ua_string)
 st.session_state.is_mobile = user_agent.is_mobile
-
+st.info(st.session_state.is_mobile)
 
 if st.session_state.is_mobile:
-    folium_static(m, width=800, height=750)
+    folium_static(m, width=600, height=750)
 
 else:
     folium_static(m, width=1200, height=750)
