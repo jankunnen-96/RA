3acc31641dcc09a760203e04e1a95372100040ed
jankunnen-96 <76209402+jankunnen-96@users.noreply.github.com>
2025-03-11 13:53:54 +0100

    t

--- pages/Whats_new.py
+++ pages/Whats_new.py
@@ -126,7 +126,7 @@ for i, row in events.iterrows():
         .event-artists { font-size: 14px; color: blue; }
 
         /* Adjust font size for tablets & mobile screens */
-        @media only screen and (max-width: 500spx) {
+        @media only screen and (max-width: 500px) {
             .event-container {
                 flex-direction: row !important;  /* Force row on mobile */
                 align-items: center;
