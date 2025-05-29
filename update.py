727811b3aa4d0ebfb66ab4d4dff5bbd5d52b4bdd
jankunnen-96 <76209402+jankunnen-96@users.noreply.github.com>
2025-05-29 22:01:00 +0200

    Update update.py

--- update.py
+++ update.py
@@ -2,9 +2,7 @@ from api import get_events_followed_profiles
 import pandas as pd
 import numpy as np
 
+get_events_followed_profiles()
 
-df_events = get_events_followed_profiles()
-
-df_events.to_csv("events2.csv", index=False)
 
 
