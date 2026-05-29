95ae96a219e6ac312d0f31aed7e20737ac000308
Jan Kunnen <jan.kunnen@stedin.net>
2026-05-29 11:08:46 +0200

    moving page

---
+++ Eventmap.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="MatchaDaddy selects💚", page_icon="🚀")
+
+st.title("MatchaDaddy has moved!")
+st.markdown("Visit the new home at [matchadaddy.onrender.com](https://matchadaddy.onrender.com/)")
+
+
+
+
