9a589e1474b3add4e016828cee5e2bdad64c40de
JAN KUNNEN <jan.kunnen@sia-partners.com>
2026-09-15 19:45:46 +0200

    update

--- frontend/src/pages/MapView.jsx
+++ frontend/src/pages/MapView.jsx
@@ -131,7 +131,7 @@ export default function MapView() {
         zoomControl={false}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url={`https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?key=${import.meta.env.VITE_CARTO_API_KEY}`}
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
         <MarkerClusterGroup chunkedLoading iconCreateFunction={clusterIcon}>
